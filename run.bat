24a4e5fcfe632b0d3bb12c8a778d6423449fb518
ndyumin <nikita.dyumin@gmail.com>
2016-01-12 07:59:25 +0500

    update run.bat with the new dep (jsx)

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 erl ^
  -sname webpackman ^
  -pa ebin ^
- -pa deps/cowboy/ebin ^
- -pa deps/gproc/ebin ^
- -pa /deps/cowlib/ebin ^
- -pa /deps/ranch/ebin ^
+ -pa ./deps/gproc/ebin ^
+ -pa ./deps/ranch/ebin ^
+ -pa ./deps/cowlib/ebin ^
+ -pa ./deps/cowboy/ebin ^
+ -pa ./deps/jsx/ebin ^
  -boot start_sasl ^
  -s boot
