9e315aa6f1316b73634116b80e99a2e063da3bd1
ndyumin <nikita.dyumin@gmail.com>
2016-01-22 09:08:06 +0500

    update static resources

--- static/bundle.js
+++ static/bundle.js
@@ -74,9 +74,22 @@
 	    return { map: mapRender };
 	}
 	
+	function parseQuery(query) {
+	    var data = {};
+	    if (query) {
+	        const pairs = query.substr(1).split('&');
+	        pairs.forEach(pair => {
+	            const param = pair.split('=');
+	            data[param[0]] = param[1];
+	        });
+	    }
+	    return data;
+	}
+	
 	(function () {
 	    const current = document.getElementById('current');
-	    const ws = new WebSocket('ws://fierce-basin-86946.herokuapp.com/websocket'),
+	    const debug = parseQuery(window.location.search)['debug'];
+	    const ws = new WebSocket(debug ? 'ws://localhost:8080/websocket' : 'ws://fierce-basin-86946.herokuapp.com/websocket'),
 	          game = gameStarter(document.getElementById('game'));
 	
 	    ws.onopen = function () {
@@ -136,13 +149,63 @@
 	    context.quadraticCurveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize, x * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
 	};
 	
+	const drawTopLeftLine = (context, x, y) => {
+	    context.moveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, y * CONSTANTS.GENERAL.blockSize);
+	    context.quadraticCurveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize, x * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawTopRightLine = (context, x, y) => {
+	    context.moveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, y * CONSTANTS.GENERAL.blockSize);
+	    context.quadraticCurveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize, (x + 1) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawTopCenterLine = (context, x, y) => {
+	    context.moveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, y * CONSTANTS.GENERAL.blockSize);
+	    context.lineTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawBottomCenterLine = (context, x, y) => {
+	    context.moveTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	    context.lineTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 1) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawLeftCenterLine = (context, x, y) => {
+	    context.moveTo(x * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	    context.lineTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawRightCenterLine = (context, x, y) => {
+	    context.moveTo((x + 1) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	    context.lineTo((x + 0.5) * CONSTANTS.GENERAL.blockSize, (y + 0.5) * CONSTANTS.GENERAL.blockSize);
+	};
+	
+	const drawBottomCurve = (context, x, y) => {
+	    drawBottomLeftLine(context, x, y);
+	    drawBottomRightLine(context, x, y);
+	};
+	
+	const drawLeftCurve = (context, x, y) => {
+	    drawBottomLeftLine(context, x, y);
+	    drawTopLeftLine(context, x, y);
+	};
+	
+	const drawRightCurve = (context, x, y) => {
+	    drawBottomRightLine(context, x, y);
+	    drawTopRightLine(context, x, y);
+	};
+	
+	const drawTopCurve = (context, x, y) => {
+	    drawTopRightLine(context, x, y);
+	    drawTopLeftLine(context, x, y);
+	};
+	
 	const renderNonTraversable = (context, x, y, block) => {
 	    context.beginPath();
 	    context.strokeStyle = CONSTANTS.WALLS_OPTION.strokeStyle;
 	    context.lineWidth = CONSTANTS.WALLS_OPTION.lineWidth;
 	    context.lineCap = CONSTANTS.WALLS_OPTION.lineCap;
 	
-	    const action = R.cond([[R.equals(CONSTANTS.WALLS.HORIZONTAL_LINE), R.always(drawHorizontalLine)], [R.equals(CONSTANTS.WALLS.VERTICAL_LINE), R.always(drawVerticalLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_RIGHT_LINE), R.always(drawBottomRightLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_LEFT_LINE), R.always(drawBottomLeftLine)], [R.T, R.always(msg => console.log(msg))]])(block);
+	    const action = R.cond([[R.equals(CONSTANTS.WALLS.HORIZONTAL_LINE), R.always(drawHorizontalLine)], [R.equals(CONSTANTS.WALLS.VERTICAL_LINE), R.always(drawVerticalLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_RIGHT_LINE), R.always(drawBottomRightLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_LEFT_LINE), R.always(drawBottomLeftLine)], [R.equals(CONSTANTS.WALLS.TOP_LEFT_LINE), R.always(drawTopLeftLine)], [R.equals(CONSTANTS.WALLS.TOP_RIGHT_LINE), R.always(drawTopRightLine)], [R.equals(CONSTANTS.WALLS.TOP_CENTER_LINE), R.always(drawTopCenterLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_CENTER_LINE), R.always(drawBottomCenterLine)], [R.equals(CONSTANTS.WALLS.LEFT_CENTER_LINE), R.always(drawLeftCenterLine)], [R.equals(CONSTANTS.WALLS.RIGHT_CENTER_LINE), R.always(drawRightCenterLine)], [R.equals(CONSTANTS.WALLS.BOTTOM_CURVE), R.always(drawBottomCurve)], [R.equals(CONSTANTS.WALLS.LEFT_CURVE), R.always(drawLeftCurve)], [R.equals(CONSTANTS.WALLS.RIGHT_CURVE), R.always(drawRightCurve)], [R.equals(CONSTANTS.WALLS.TOP_CURVE), R.always(drawTopCurve)], [R.T, R.always(msg => console.log(msg))]])(block);
 	    action(context, x, y);
 	    context.stroke();
 	};
@@ -8631,7 +8694,17 @@
 	    HORIZONTAL_LINE: 200,
 	    VERTICAL_LINE: 201,
 	    BOTTOM_RIGHT_LINE: 202,
-	    BOTTOM_LEFT_LINE: 203
+	    BOTTOM_LEFT_LINE: 203,
+	    TOP_RIGHT_LINE: 204,
+	    TOP_LEFT_LINE: 205,
+	    TOP_CENTER_LINE: 206,
+	    BOTTOM_CENTER_LINE: 207,
+	    LEFT_CENTER_LINE: 208,
+	    RIGHT_CENTER_LINE: 209,
+	    BOTTOM_CURVE: 210,
+	    TOP_CURVE: 211,
+	    LEFT_CURVE: 212,
+	    RIGHT_CURVE: 213
 	};
 	
 	module.exports = {
