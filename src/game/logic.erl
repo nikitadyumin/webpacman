5cb2f6dc1f547763baaa62485c1d037103441c0c
ndyumin <nikita.dyumin@gmail.com>
2016-01-24 10:19:02 +0500

    rewrite update with guards

--- src/game/logic.erl
+++ src/game/logic.erl
@@ -23,19 +23,20 @@ add_player(Connection) ->
   gproc:send({r, l, messenger}, {send_map, Connection}).
 
 update_position(Connection, Position) ->
-  gproc:send({r, l, map}, {get_at, Position,
-    fun(Tile) ->
-      if
-        Tile == 140 -> % bonus
-          gproc:send({r, l, map}, {set, Position, 100}),
-          gproc:send({r, l, players}, {add_score, Connection, ?BONUS}),
-          gproc:send({r, l, players}, {update, Connection, Position}),
-          [MapPid| _] = gproc:lookup_pids({r, l, map}),
-          erlang:send_after(?BONUS_RESTORE_TIME_MS, MapPid, {set, Position, 140});
-        Tile >= 100 andalso Tile < 200 -> % generally traversable
-          gproc:send({r, l, players}, {update, Connection, Position});
-        true ->
-          erlang:display(<<"illigal position">>)
-      end
-    end
-  }).
+  gproc:send({r, l, map}, {get_at, Position, curry_update(Connection, Position)}).
+
+curry_update(Connection, Position) ->
+  fun (TileCode) -> update(Connection, Position, TileCode) end.
+
+update(Connection, Position, TileCode) when TileCode == 140 -> % bonus
+  gproc:send({r, l, map}, {set, Position, 100}),
+  gproc:send({r, l, players}, {add_score, Connection, ?BONUS}),
+  gproc:send({r, l, players}, {update, Connection, Position}),
+  [MapPid | _] = gproc:lookup_pids({r, l, map}),
+  erlang:send_after(?BONUS_RESTORE_TIME_MS, MapPid, {set, Position, 140});
+
+update(Connection, Position, TileCode) when TileCode >= 100 andalso TileCode < 200 -> % generally traversable
+gproc:send({r, l, players}, {update, Connection, Position});
+
+update(_Connection, _Position, _TileCode) ->
+  erlang:display(<<"illigal position">>).
