ad5fbb01e755788ad07e3fb23883fbb4cb40a37b
ndyumin <nikita.dyumin@gmail.com>
2016-02-13 18:34:50 +0500

    add missing file

---
+++ client/src/view/stats.js
@@ -0,0 +1,41 @@
+/**
+ * Created by ndyumin on 13.02.2016.
+ */
+import h from 'virtual-dom/h';
+import patch from 'virtual-dom/patch';
+import diff from 'virtual-dom/diff';
+import createElement from 'virtual-dom/create-element';
+
+const statsModal = state =>
+    h('div', {className: 'modal', style: {'display': state ? 'block' : 'none'}},
+        h('table', {},
+            h('tbody', {},
+                h('tr', {},
+                    h('td', {}, 'skjdhf'),
+                    h('td', {}, 'skjdhf'),
+                    h('td', {}, 'skjdhf')
+                ),
+                h('tr', {},
+                    h('td', {}, '34f43'),
+                    h('td', {}, '3f'),
+                    h('td', {}, '3f34f')
+                ),
+                h('tr', {},
+                    h('td', {}, '034kf'),
+                    h('td', {}, '394jf'),
+                    h('td', {}, '3j4f8j')
+                )
+            ))
+    );
+
+
+const EMPTY_TREE = h('div');
+
+const getUpdater = model$ =>
+    model$
+        .map(statsModal)
+        .scan((tree, newTree) => diff(tree, newTree), EMPTY_TREE);
+
+module.exports = {
+    getUpdater
+};
